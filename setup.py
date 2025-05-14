9b03cdcd890cfe6843504454244646378f432a6c
alrxkali <accual1013@gmail.com>
2025-05-14 15:56:18 -0600

    Remove torch local dependency

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
-import torch
-import torchvision
 from setuptools import setup, find_packages
 
-torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
-assert torch_ver >= [1, 9], "Requires PyTorch >= 1.9"
-torchvision_ver = [int(x) for x in torchvision.__version__.split(".")[:2]]
-assert torchvision_ver >= [0, 11], "Requires torchvision >= 0.11"
+# torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
+# assert torch_ver >= [1, 9], "Requires PyTorch >= 1.9"
+# torchvision_ver = [int(x) for x in torchvision.__version__.split(".")[:2]]
+# assert torchvision_ver >= [0, 11], "Requires torchvision >= 0.11"
 
 setup(
     name="dp2",
